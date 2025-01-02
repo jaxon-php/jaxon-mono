4676c2444de81ca19015302b68ba570ecf53a731
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2025-01-02 16:46:35 +0100

    Updated the js lib version.

--- jaxon-examples/templates/buttons/ext.php
+++ jaxon-examples/templates/buttons/ext.php
@@ -1,2 +1,2 @@
-<button type="button" class="btn btn-primary" <?php echo Jaxon\attr()->click($this->test->sayHello(1)) ?>>CLICK ME</button>
-<button type="button" class="btn btn-primary" <?php echo Jaxon\attr()->click($this->test->sayHello(0)) ?> >Click Me</button>
+<button type="button" class="btn btn-primary" <?php echo Jaxon\attr()->click($this->test->sayHello(true)) ?>>CLICK ME</button>
+<button type="button" class="btn btn-primary" <?php echo Jaxon\attr()->click($this->test->sayHello(false)) ?> >Click Me</button>
