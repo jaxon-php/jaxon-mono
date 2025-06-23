601bfcb05c6aae4e18f2241ff734225cc66fe82b
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2025-06-24 01:51:28 +0200

    New monolog logger in the examples app.

--- jaxon-examples/examples/bootstrap.php
+++ jaxon-examples/examples/bootstrap.php
@@ -1,8 +1,19 @@
 <?php
 
+use Monolog\Level;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
 require dirname(__DIR__, 2) . '/vendor/autoload.php';
 require_once dirname(__DIR__, 2) . '/jaxon-core/src/globals.php';
 require_once __DIR__ . '/menu.php';
+
+// Register the logger
+jaxon()->setLogger(function() {
+    $logFile = dirname(__DIR__) . '/logs/examples.log';
+    return (new Logger('examples'))
+        ->pushHandler(new StreamHandler($logFile, Level::Debug));
+});
 
 jaxon()->template()
     ->addNamespace('examples', __DIR__)
